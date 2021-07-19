1f7e1123d732ddea01abac510f0a8db61077c069
Smartfeld.ch <78407877+smartfeld-ch@users.noreply.github.com>
2021-07-19 15:27:32 +0200

    Alarm bei Licht

--- main.ts
+++ main.ts
@@ -1,3 +1,32 @@
+
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    if (Schalter) {
+        Schalter = 0
+    } else {
+        Schalter = 1
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        basic.showString("aktiv")
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.lightLevel())
+})
+let Schalter = 0
+Schalter = 0
 basic.forever(function () {
-	
+    if (Schalter) {
+        if (input.lightLevel() > 80) {
+            basic.showIcon(IconNames.Angry)
+            music.startMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Once)
+        } else {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+        }
+        basic.pause(2000)
+    }
 })
